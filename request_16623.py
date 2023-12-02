be73491e3983d55390b994c0d4eb69cca85356da
smart flex <smartflex@flex24bot-1.us-east4-c.c.rugged-link-395416.internal>
2023-12-01 22:05:10 -0600

    save

--- request_16623.py
+++ request_16623.py
@@ -1,5 +1,4 @@
 import time
-
 import requests
 import PyChromeDevTools
 import requests
@@ -9,31 +8,43 @@ from database2 import *
 import json
 import subprocess
 
-# command = "google-chrome --user-data-dir=$HOME/docinfo --remote-debugging-port=9222 --remote-allow-origins=http://localhost:9222"
-command = "google-chrome --user-data-dir=$HOME/16623 --proxy-server=138.197.224.38:16623 --remote-debugging-port=16623 --remote-allow-origins=http://localhost:16623"
-chrome_process = subprocess.Popen(command, shell=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL,
-                                  stderr=subprocess.DEVNULL, close_fds=True)
-time.sleep(1)
+
+proxy_to_test = '65.21.25.28:1039:9BeAXC3urY:ZFyLXaE14k'
 
 
-def get_cookie(chrome_, change_ip):
-    while change_ip:
-        burp0_url = "https://gridpanel.net:443/api/reboot?token=NjY2OSw4MjA5ZTk1My1kYWJkLTQ4ZTYtOTA0ZC1iZjc1YzJiNDYzZWQ%3D"
-        burp0_headers = {"Sec-Ch-Ua": "\"Chromium\";v=\"117\", \"Not;A=Brand\";v=\"8\"", "Sec-Ch-Ua-Mobile": "?0",
-                         "Sec-Ch-Ua-Platform": "\"Linux\"", "Upgrade-Insecure-Requests": "1",
-                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.63 Safari/537.36",
-                         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-                         "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
-                         "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate, br",
-                         "Accept-Language": "en-US,en;q=0.9", "Connection": "close"}
-        res = requests.get(burp0_url, headers=burp0_headers)
-        if res.status_code == 200:
-            print(f"ip changed successfully: {res.text}")
-            time.sleep(30)
-            break
-        else:
-            time.sleep(10)
-            continue
+def test_proxy(proxy, timeout=5):
+    proxy_live_status: bool = False
+    while not proxy_live_status:
+        try:
+            proxy_parts = proxy.split(':')
+            ip_address = proxy_parts[0]
+            port = proxy_parts[1]
+            username = proxy_parts[2]
+            password = proxy_parts[3]
+            session = requests.Session()
+            session.proxies = {'http': f'http://{username}:{password}@{ip_address}:{port}',
+                               'https': f'http://{username}:{password}@{ip_address}:{port}'}
+            response = session.get('https://www.jsonip.com', timeout=timeout)
+            if response.status_code == 200:
+                print(f"The proxy {proxy} is live: {response.json()['ip']}")
+                return response.json()['ip']
+            else:
+                time.sleep(1)
+                print(f"The proxy {proxy} returned a non-200 status code: {response.status_code}")
+                proxy_live_status = False
+        except requests.RequestException as e:
+            time.sleep(1)
+            print(f"Error testing proxy {proxy}: {e}")
+            proxy_live_status = False
+
+
+def get_cookie(chrome_, ip_):
+    _ip = test_proxy(proxy_to_test, 5)
+    while ip_ == _ip:
+        _ip = test_proxy(proxy_to_test, 5)
+        time.sleep(2)
+        print(f"ip_: {ip_} and _ip: {_ip}")
+
     url = "https://www.docinfo.org:443/search/docprofile?docid=7C468AA8-A15F-4951-A14F-07119858FD7D&token="
     chrome_.Page.navigate(url=url)
     event, messages = chrome_.wait_event("Page.frameStoppedLoading", timeout=60)
@@ -45,7 +56,11 @@ def get_cookie(chrome_, change_ip):
                     }
                     getDocumentSource();
                 """
+    while_count = 0
     while True:
+        while_count += 1
+        if while_count > 20:
+            break
         try:
             result = chrome_.Runtime.evaluate(expression=get_page_source_js)
             html_source_listing = result[0]['result']['result']['value']
@@ -56,22 +71,88 @@ def get_cookie(chrome_, change_ip):
                 if iframe_src_ is None:
                     break
                 if '_Incapsula_Resource' in iframe_src_:
-                    print('captcha appeared, so solve it manually')
+                    print(f'{while_count}: captcha appeared, so solve it manually')
                     time.sleep(3)
                     continue
                 else:
-                    break
+                    cookies = chrome_.Network.getAllCookies()
+                    for cookie in cookies[0]['result']['cookies']:
+                        if cookie['name'] == 'reese84':
+                            token_value = cookie['value']
+                            return token_value
+                    # break
             else:
-                break
+                cookies = chrome_.Network.getAllCookies()
+                for cookie in cookies[0]['result']['cookies']:
+                    if cookie['name'] == 'reese84':
+                        token_value = cookie['value']
+                        return token_value
+                # break
         except Exception as e:
             print(e)
-    cookies = chrome_.Network.getAllCookies()
-    for cookie in cookies[0]['result']['cookies']:
-        if cookie['name'] == 'reese84':
-            token_value = cookie['value']
-            return token_value
 
-    # chrome.Browser.close()
+# command = "google-chrome --user-data-dir=$HOME/docinfo --remote-debugging-port=9222 --remote-allow-origins=http://localhost:9222"
+command = "google-chrome --user-data-dir=$HOME/16623 --proxy-server=65.21.25.28:1039 --remote-debugging-port=16623 --remote-allow-origins=http://localhost:16623"
+chrome_process = subprocess.Popen(command, shell=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, close_fds=True)
+time.sleep(1)
+
+
+# def get_cookie(chrome_, change_ip):
+#     while change_ip:
+#         burp0_url = "https://gridpanel.net:443/api/reboot?token=NjY2OSw4MjA5ZTk1My1kYWJkLTQ4ZTYtOTA0ZC1iZjc1YzJiNDYzZWQ%3D"
+#         burp0_headers = {"Sec-Ch-Ua": "\"Chromium\";v=\"117\", \"Not;A=Brand\";v=\"8\"", "Sec-Ch-Ua-Mobile": "?0",
+#                          "Sec-Ch-Ua-Platform": "\"Linux\"", "Upgrade-Insecure-Requests": "1",
+#                          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.63 Safari/537.36",
+#                          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+#                          "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
+#                          "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate, br",
+#                          "Accept-Language": "en-US,en;q=0.9", "Connection": "close"}
+#         res = requests.get(burp0_url, headers=burp0_headers)
+#         if res.status_code == 200:
+#             print(f"ip changed successfully: {res.text}")
+#             time.sleep(30)
+#             break
+#         else:
+#             time.sleep(10)
+#             continue
+#     url = "https://www.docinfo.org:443/search/docprofile?docid=7C468AA8-A15F-4951-A14F-07119858FD7D&token="
+#     chrome_.Page.navigate(url=url)
+#     event, messages = chrome_.wait_event("Page.frameStoppedLoading", timeout=60)
+#     value = chrome_.wait_event("Network.responseReceived", timeout=60)
+#
+#     get_page_source_js = """
+#                     function getDocumentSource() {
+#                         return document.documentElement.outerHTML;
+#                     }
+#                     getDocumentSource();
+#                 """
+#     while True:
+#         try:
+#             result = chrome_.Runtime.evaluate(expression=get_page_source_js)
+#             html_source_listing = result[0]['result']['result']['value']
+#             soup_ = BeautifulSoup(html_source_listing, 'html.parser')
+#             iframe_ = soup_.find('iframe')
+#             if iframe_:
+#                 iframe_src_ = iframe_.get('src')
+#                 if iframe_src_ is None:
+#                     break
+#                 if '_Incapsula_Resource' in iframe_src_:
+#                     print('captcha appeared, so solve it manually')
+#                     time.sleep(3)
+#                     continue
+#                 else:
+#                     break
+#             else:
+#                 break
+#         except Exception as e:
+#             print(e)
+#     cookies = chrome_.Network.getAllCookies()
+#     for cookie in cookies[0]['result']['cookies']:
+#         if cookie['name'] == 'reese84':
+#             token_value = cookie['value']
+#             return token_value
+#
+#     # chrome.Browser.close()
 
 
 break_while = True
@@ -81,7 +162,7 @@ while break_while:
     chrome.Network.enable()
     chrome.Page.enable()
     chrome.DOM.enable()
-    reese84 = get_cookie(chrome, False)
+    reese84 = get_cookie(chrome, "127.0.0.1")
     print(reese84)
     all_doc_id = get_doc_id()
     n = 0
@@ -105,7 +186,7 @@ while break_while:
 
         burp0_headers = {"Sec-Ch-Ua": "\"Chromium\";v=\"112\", \"Not;A=Brand\";v=\"8\"", "Sec-Ch-Ua-Mobile": "?0",
                          "Sec-Ch-Ua-Platform": "\"Linux\"", "Upgrade-Insecure-Requests": "1",
-                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.5938.63 Safari/537.36",
+                         "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
                          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                          "Sec-Fetch-Site": "none", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1",
                          "Sec-Fetch-Dest": "document", "Accept-Encoding": "gzip, deflate, br",
@@ -114,12 +195,8 @@ while break_while:
         session.headers = {}
         session.headers = burp0_headers
         try:
-            res = session.get(
-                burp0_url,
-                proxy="http://138.197.224.38:16623",
-                cookies=burp0_cookies,
-                timeout_seconds=35
-            )
+            ip = test_proxy(proxy_to_test, 5, )
+            res = session.get(burp0_url, proxy="http://9BeAXC3urY:ZFyLXaE14k@65.21.25.28:1039", cookies=burp0_cookies, timeout_seconds=35)
             print(burp0_url)
             # proxy = "http://KlbNcNG3nZ:DoYXg5YHlx@65.21.25.28:1037",
             html_source = res.text
@@ -130,7 +207,7 @@ while break_while:
                 iframe_src = iframe.get('src')
                 if '_Incapsula_Resource' in iframe_src:
                     print('captcha appeared, so solve it manually. now switching to browser')
-                    reese84 = get_cookie(chrome, True)
+                    reese84 = get_cookie(chrome, ip)
                     time.sleep(3)
                     continue
                 else:
